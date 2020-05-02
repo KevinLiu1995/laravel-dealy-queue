8b0c3430735f93b6c8446784e676b83df4e22e3f
Kevin <863129201@qq.com>
2020-05-02 22:42:31 +0800

    补充抽奖通知

--- app/Http/Controllers/ChouJiangController.php
+++ app/Http/Controllers/ChouJiangController.php
@@ -33,8 +33,8 @@ class ChouJiangController extends Controller
         // 计算给定的时间与当前时间的差值，得出延迟执行的时间
         $int = (new Carbon)->diffInSeconds($carbon, false);
         // 分发队列，传入延迟执行时间，等待执行
-//        $this->dispatch(new TestJob($int, $request->email));
-        $this->dispatch(new TestJob(5, $request->email));
+        $this->dispatch(new TestJob($int, $request->email));
+//        $this->dispatch(new TestJob(5, $request->email));
 
         //格式化时间显示，用于提示
         $delay = $carbon->diffForHumans(Carbon::now());
