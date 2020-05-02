151c028301580c2159a54a8919610f3131acb5fe
Kevin <863129201@qq.com>
2020-05-02 21:38:22 +0800

    更新域名配置

--- config/horizon.php
+++ config/horizon.php
@@ -15,7 +15,7 @@ return [
     |
     */
 
-    'domain' => "http://demoapi.kevinliu.top/horizon",
+    'domain' => "demoapi.kevinliu.top",
 
     /*
     |--------------------------------------------------------------------------
